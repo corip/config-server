0180c79b2a918ec173d81c6c996cd1f688122d4e
unknown <coripedro3@gmail.com>
2021-05-07 08:41:17 -0500

    configserver

---
+++ configserver/src/test/java/com/bootcamp/hack/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.hack.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
